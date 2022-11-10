408b2d3c8b1e6977169885b7e9e55d4319ed76d4
chidijacob <chidijacob54@gmail.com>
2022-11-10 06:30:10 +0100

    Add existing project files to Git

--- app.js
+++ app.js
@@ -97,3 +97,25 @@ function binarySearch(arr, t) {
 // console.log(binarySearch([-5,2,4,6,10], 10));
 // console.log(binarySearch([-5,2,4,6,10], 6));
 // console.log(binarySearch([-5,2,4,6,10], 20));
+
+function recursiveBinarySearch(arr, target) {
+    return search(arr, target, 0, arr.length-1)
+}
+
+function search(arr, target, leftIndex, rightIndex) {
+    if(leftIndex > rightIndex) {
+        return -1
+    }
+    let middleIndex = Math.floor((leftIndex + rightIndex)/2)
+    if(target===arr[middleIndex]) {
+        return middleIndex
+    }
+    if(target<arr[middleIndex]) {
+        return search (arr,target,leftIndex,middleIndex-1)
+    } else {
+        return search(arr, target, middleIndex+1, rightIndex)
+    }
+}
+console.log(binarySearch([-5,2,4,6,10], 10));
+console.log(binarySearch([-5,2,4,6,10], 6));
+console.log(binarySearch([-5,2,4,6,10], 20));
